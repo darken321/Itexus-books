df10340a148766359cf58ddd4bbdf496ca3db8b3
Vasili Bandarenka <darken123@inbox.ru>
2024-10-20 10:58:06 +0300

    добавлено логирование значения параметров вызова и выходное значение всех сервисных методов

--- src/main/java/org/example/aop/LoggingAspect.java
+++ src/main/java/org/example/aop/LoggingAspect.java
@@ -1,8 +1,12 @@
 package org.example.aop;
 
+
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,8 +14,42 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class LoggingAspect {
 
-    @Before("execution(* org.example.service.*.*(..))")
+    @Pointcut("within(org.example.service..*)")
+    public void servicePointcut() {
+    }
+
+    /**
+     * Логирует вызов любого сервисного метода в пакете org.example.service.
+     */
+    @Before("servicePointcut()")
     public void logBeforeService() {
         log.info("Вызов сервисного метода");
+    }
+
+    /**
+     * Логирует параметры вызова и выходное значение методов в пакете org.example.service.
+     *
+     * @param joinPoint объект, представляющий точку соединения (вызов метода)
+     * @return результат выполнения метода
+     * @throws Throwable если метод выбрасывает исключение
+     */
+    @Around("servicePointcut()")
+    public Object logAroundService(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        String methodName = joinPoint.getSignature().toShortString();
+        Object[] args = joinPoint.getArgs();
+        log.info("Вызов метода: {} с аргументами: {}", methodName, args);
+
+        Object result;
+        try {
+            result = joinPoint.proceed();
+        } catch (Throwable throwable) {
+            log.error("Исключение в методе: {} с аргументами: {}", methodName, args, throwable);
+            throw throwable;
+        }
+
+        log.info("Метод {} вернул значение {}", methodName, result);
+
+        return result;
     }
 }
